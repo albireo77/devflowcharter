b84a4a39313ed346ffe0daa1496a04d8d1543a62
albireo77 <albireo77@users.noreply.github.com>
2024-09-14 12:14:52 +0200

    Update BlockTabSheet.pas

--- Common/BlockTabSheet.pas
+++ Common/BlockTabSheet.pas
@@ -25,7 +25,7 @@ interface
 
 uses
    System.Classes, Vcl.ComCtrls, Vcl.Controls, Vcl.Forms, WinApi.Messages, System.Types,
-   Vcl.Graphics, Interfaces, Main_Form, OmniXML;
+   Vcl.Graphics, Main_Form, OmniXML;
 
 type
 
@@ -68,7 +68,7 @@ implementation
 
 uses
    System.SysUtils, System.StrUtils, System.Math, System.UITypes, WinApi.Windows,
-   UserFunction, Navigator_Form, OmniXMLUtils, Infrastructure, Constants;
+   Navigator_Form, OmniXMLUtils, Infrastructure, Constants;
 
 constructor TBlockTabSheet.Create(AMainForm: TMainForm);
 begin
