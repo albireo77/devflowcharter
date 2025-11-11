30b6cc0100d331f1f5217794240b934992f63f99
albireo77 <albireo77@users.noreply.github.com>
2025-11-11 18:04:45 +0100

    Updates

--- Common/Element.pas
+++ Common/Element.pas
@@ -165,7 +165,7 @@ begin
    end;
    edtName.Font.Color := lColor;
    edtName.Hint := trnsManager.GetString(info);
-   if IsValid then
+   if (edtName.Font.Color = OK_COLOR) or not edtName.Focused then
       FParentTab.PageControl.Refresh;
    GProject.SetChanged;
    if FParentForm.UpdateCodeEditor then
